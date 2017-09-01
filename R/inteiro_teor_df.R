c64409ad736f357d1a27679b57ad908a5f9189dc
José de Jesus Filho <jjesusfilho@gmail.com>
2017-09-01 04:48:39 -0300

    Update inteiro_teor_df.R

--- R/inteiro_teor_df.R
+++ R/inteiro_teor_df.R
@@ -10,61 +10,39 @@
 #' inteiro_teor_df(registro)
 
 
-inteiro_teor_df<-function(registro){
-  set_config(config(ssl_verifypeer = FALSE))
-  
-  body<-list(visaoId = "tjdf.sistj.acordaoeletronico.buscaindexada.apresentacao.VisaoBuscaAcordao", 
-             controladorId = "tjdf.sistj.acordaoeletronico.buscaindexada.apresentacao.ControladorBuscaAcordao", 
-             idDoUsuarioDaSessao = "", nomeDaPagina = "dadosDoAcordao", 
-             comando = "downloadInteiroTeor", enderecoDoServlet = "sistj", 
-             visaoAnterior = "tjdf.sistj.acordaoeletronico.buscaindexada.apresentacao.VisaoBuscaAcordao", 
-             skin = "", tokenDePaginacao = "1", historicoDePaginas = "buscaLivre", 
-             historicoDePaginas = "resultado", argumentoDePesquisa = "", 
-             hppert = "", numero = "", tipoDeNumero = "", tljjat = "", 
-             desembargador = "", kahufx = "", tipoDeRelator = "", orgaoJulgador = "", 
-             ementa = "", decisaoLivre = "", indexacao = "", talkls = "", 
-             tipoDeData = "", tipoDeBusca = "", camposSelecionados = "ESPELHO", 
-             camposDeAgrupamento = "", numeroDoDocumento = "", 
-             quantidadeDeRegistros = "20", dataInicio = "", dataFim = "", 
-             campoDeOrdenacao = "", baseDados = "BASE_ACORDAOS", baseDados = "TURMAS_RECURSAIS", 
-             baseDados = "BASE_ACORDAO_PJE", baseDados = "BASE_HISTORICA", 
-             numeroDaUltimaPagina = "1", idDespacho = "", buscaIndexada = "1", 
-             mostrarPaginaSelecaoTipoResultado = "false", totalHits = "1", 
-             tipoDeDocumento = "", query = "{}", buscaPorQuery = "", argumentoDePesquisa = "", 
-             jurisprudenciaAdministrativa = "", faixaInicial = "", faixaFinal = "", 
-             indexados = "", decisao = "", decisaoLivre = "", loginDoUsuario = "", 
-             nomeDoUsuario = "", idClasse = "", descricaoClasse = "", 
-             qrwywx = "", indiceAcordaoAtual = "0", numeroDoDocumento = "", 
-             numeroDaPaginaAtual = "1", caminhoDoAcordao = "", baseSelecionada = "BASE_ACORDAO_TODAS", 
-             quantidadeDeRegistros = "20", numeroDaUltimaPagina = "1", 
-             totalHits = "1", tipoDeDocumento = "", jurisprudenciaAdministrativa = "", 
-             faixaInicial = "", faixaFinal = "", indexados = "", decisao = "", 
-             decisaoLivre = "", loginDoUsuario = "", nomeDoUsuario = "", 
-             idClasse = "", descricaoClasse = "", idDoAcordao = "", viqoev = "aberto", 
-             idJanelaAbrirAjax = "", idJanelaAbrirIsModalAjax = "false", 
-             internet = "1", iebzrz = "<?xml version='1.0' encoding='ISO-8859-1'?><comando><camposParaRedesenhar></camposParaRedesenhar><camposParaRecarregar><campo>idJanelaAbrirAjax</campo><campo>idJanelaAbrirIsModalAjax</campo></camposParaRecarregar><campoComMensagem></campoComMensagem><timeoutDeMensagem>0</timeoutDeMensagem><campoComFoco></campoComFoco><comandoDeSaida></comandoDeSaida></comando>")
-  
-  
-  for (i in seq_along(registro)){
-    tryCatch({
-      body$numeroDoDocumento<-registro[i]
-      s<-POST("http://pesquisajuris.tjdft.jus.br/IndexadorAcordaos-web/sistj",body=body)
-      
-      idd <-  s %>%  httr::content('text') %>% 
-        stringr::str_extract('idd=[0-9a-zA-Z]+')
-      
-      u_teor <- paste0(
-        "http://pesquisajuris.tjdft.jus.br/IndexadorAcordaos-web/infra/Download.jsp?",
-        idd)
-      
-      httr::GET(u_teor, httr::write_disk(paste0(registro[i],".pdf"), overwrite = TRUE))
-    }, error = function(m) {
-      m
-    }, finally = {
-      next
-    })
-    Sys.sleep(0.5)
+inteiro<-function(acordao){
+
+for(i in seq_along(acordao)){
+  tryCatch({
     
-  }
-  
+u <- "http://pesquisajuris.tjdft.jus.br/IndexadorAcordaos-web/sistj?visaoId=tjdf.sistj.acordaoeletronico.buscaindexada.apresentacao.VisaoBuscaAcordao&controladorId=tjdf.sistj.acordaoeletronico.buscaindexada.apresentacao.ControladorBuscaAcordao&visaoAnterior=tjdf.sistj.acordaoeletronico.buscaindexada.apresentacao.VisaoBuscaAcordao&nomeDaPagina=resultado&comando=abrirDadosDoAcordao&enderecoDoServlet=sistj&historicoDePaginas=buscaLivre&quantidadeDeRegistros=20&baseSelecionada=BASE_ACORDAO_TODAS&numeroDaUltimaPagina=1&buscaIndexada=1&mostrarPaginaSelecaoTipoResultado=false&totalHits=1&internet=1&numeroDoDocumento="
+url<-paste0(u,acordao[i])
+# usei html session pra economizar codigo de escrever o form inteiro
+s <- rvest::html_session(url)
+form <- rvest::html_form(s)[[1]] %>% 
+  rvest::set_values(comando = 'downloadInteiroTeor')
+s <- rvest::submit_form(s, form)
+
+# pulo do gato tá aqui: o download vem de uma GET request com um codigo gerado na pagina da resposta do form
+idd <- s$response %>% 
+  httr::content('text') %>% 
+  stringr::str_extract('idd=[0-9a-zA-Z]+')
+
+# agora é só correr pro abraço
+u_teor <- paste0(
+  "http://pesquisajuris.tjdft.jus.br/IndexadorAcordaos-web/infra/Download.jsp?",
+  idd
+)
+
+binario<-httr::GET(u_teor)
+
+ifelse(binario$headers$`content-type`=="application/msword",
+       writeBin(binario$content,paste0(acordao[i],".doc")),
+       writeBin(binario$content,paste0(acordao[i],".pdf")))
+  }, error = function(m) {
+    m
+  }, finally = {
+    next
+  })
+}
 }
